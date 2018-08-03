39adc51119414f1e6264641e1ba1119530cbdb46
Milan Kragujevic <milankragujevic@gmail.com>
2018-08-03 14:22:37 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,326 @@
+#!/bin/bash
+set -e
+
+## set defaults
+
+rom_fp="$(date +%y%m%d)"
+
+myname="$(basename "$0")"
+if [[ $(uname -s) = "Darwin" ]];then
+    jobs=$(sysctl -n hw.ncpu)
+elif [[ $(uname -s) = "Linux" ]];then
+    jobs=$(nproc)
+fi
+
+## handle command line arguments
+read -p "Do you want to sync? " choice 
+
+function help() {
+    cat <<EOF
+Syntax:
+
+  $myname [-j 2] <rom type> <variant>...
+
+Options:
+
+  -j   number of parallel make workers (defaults to $jobs)
+
+ROM types:
+
+  aosp80
+  aosp81
+  carbon
+  lineage
+  rr
+  pixel
+  crdroid
+  mokee
+  aicp
+  aokp
+  slim
+  aex
+
+Variants are dash-joined combinations of (in order):
+* processor type
+  * "arm" for ARM 32 bit
+  * "arm64" for ARM 64 bit
+* A or A/B partition layout ("aonly" or "ab")
+* GApps selection
+  * "vanilla" to not include GApps
+  * "gapps" to include opengapps
+  * "go" to include gapps go
+* SU selection ("su" or "nosu")
+
+for example:
+
+* arm-aonly-vanilla-nosu
+* arm64-ab-gapps-su
+EOF
+}
+
+function get_rom_type() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            aosp80)
+                mainrepo="https://android.googlesource.com/platform/manifest"
+                mainbranch="android-vts-8.0_r4"
+                localManifestBranch="master"
+                treble_generate=""
+                extra_make_options=""
+                ;;
+            aosp81)
+                mainrepo="https://android.googlesource.com/platform/manifest"
+                mainbranch="android-8.1.0_r30"
+                localManifestBranch="master"
+                treble_generate=""
+                extra_make_options=""
+                ;;
+            carbon)
+                mainrepo="https://github.com/CarbonROM/android"
+                mainbranch="cr-6.1"
+                localManifestBranch="master"
+                treble_generate="carbon"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+            lineage)
+                mainrepo="https://github.com/LineageOS/android.git"
+                mainbranch="lineage-15.1"
+                localManifestBranch="master"
+                treble_generate="lineage"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+            rr)
+                mainrepo="https://github.com/ResurrectionRemix/platform_manifest.git"
+                mainbranch="oreo"
+                localManifestBranch="master"
+                treble_generate="rr"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+            pixel)
+                mainrepo="https://github.com/PixelExperience/manifest"
+                mainbranch="oreo-mr1"
+                localManifestBranch="master"
+                treble_generate="pixel"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+            crdroid)
+                mainrepo="https://github.com/crdroidandroid/android"
+                mainbranch="8.1"
+                localManifestBranch="master"
+                treble_generate="crdroid"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+            mokee)
+                mainrepo="https://github.com/MoKee/android.git"
+                mainbranch="mko-mr1"
+                localManifestBranch="master"
+                treble_generate="mokee"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+            aicp)
+                mainrepo="https://github.com/AICP/platform_manifest.git"
+                mainbranch="o8.1"
+                localManifestBranch="master"
+                treble_generate="aicp"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+            aokp)
+                mainrepo="https://github.com/AOKP/platform_manifest.git"
+                mainbranch="oreo"
+                localManifestBranch="master"
+                treble_generate="aokp"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+            aex)
+                mainrepo="https://github.com/AospExtended/manifest.git"
+                mainbranch="8.1.x"
+                localManifestBranch="master"
+                treble_generate="aex"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+	    slim)
+                mainrepo="git://github.com/SlimRoms/platform_manifest.git "
+                mainbranch="or8.1"
+                localManifestBranch="master"
+                treble_generate="slim"
+                extra_make_options="WITHOUT_CHECK_API=true"
+                ;;
+
+
+
+
+        esac
+        shift
+    done
+}
+
+function parse_options() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -j)
+                jobs="$2";
+                shift;
+                ;;
+        esac
+        shift
+    done
+}
+
+declare -A processor_type_map
+processor_type_map[arm]=arm
+processor_type_map[arm64]=arm64
+
+declare -A partition_layout_map
+partition_layout_map[aonly]=a
+partition_layout_map[ab]=b
+
+declare -A gapps_selection_map
+gapps_selection_map[vanilla]=v
+gapps_selection_map[gapps]=g
+gapps_selection_map[go]=o
+
+declare -A su_selection_map
+su_selection_map[su]=S
+su_selection_map[nosu]=N
+
+function parse_variant() {
+    local -a pieces
+    IFS=- pieces=( $1 )
+
+    local processor_type=${processor_type_map[${pieces[0]}]}
+    local partition_layout=${partition_layout_map[${pieces[1]}]}
+    local gapps_selection=${gapps_selection_map[${pieces[2]}]}
+    local su_selection=${su_selection_map[${pieces[3]}]}
+
+    if [[ -z "$processor_type" || -z "$partition_layout" || -z "$gapps_selection" || -z "$su_selection" ]]; then
+        >&2 echo "Invalid variant '$1'"
+        >&2 help
+        exit 2
+    fi
+
+    echo "treble_${processor_type}_${partition_layout}${gapps_selection}${su_selection}-userdebug"
+}
+
+declare -a variant_codes
+declare -a variant_names
+function get_variants() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            *-*-*-*)
+                variant_codes[${#variant_codes[*]}]=$(parse_variant "$1")
+                variant_names[${#variant_names[*]}]="$1"
+                ;;
+        esac
+        shift
+    done
+}
+
+## function that actually do things
+
+function init_release() {
+    mkdir -p release/"$rom_fp"
+}
+
+function init_main_repo() {
+    repo init -u "$mainrepo" -b "$mainbranch"
+}
+
+function clone_or_checkout() {
+    local dir="$1"
+    local repo="$2"
+    
+    if [[ -d "$dir" ]];then
+        (
+            cd "$dir"
+            git fetch
+            git reset --hard
+            git checkout origin/"$localManifestBranch"
+        )
+    else
+        git clone https://github.com/atu-l21/"$repo" "$dir" -b "$localManifestBranch"
+    fi
+}
+
+function init_local_manifest() {
+    clone_or_checkout .repo/local_manifests manifest
+}
+
+function init_patches() {
+    if [[ -n "$treble_generate" ]]; then
+        clone_or_checkout patches patches
+       
+        # We don't want to replace from AOSP since we'll be applying
+        # patches by hand
+        rm -f .repo/local_manifests/replace.xml
+
+        # should I do this? will it interfere with building non-gapps images?
+        # rm -f .repo/local_manifests/opengapps.xml
+    fi
+}
+
+function sync_repo() {
+    repo sync -c -j "$jobs" --force-sync
+}
+
+function patch_things() {
+    if [[ -n "$treble_generate" ]]; then
+        rm -f device/*/sepolicy/common/private/genfs_contexts
+        (
+            cd device/huawei/atu_l21
+    if [[ $choice == *"y"* ]];then
+            git clean -fdx
+    fi
+            bash generate.sh "$treble_generate"
+        )
+        bash "$(dirname "$0")/apply-patches.sh" patches
+    else
+        (
+            cd device/huawei/atu_l21
+            git clean -fdx
+            bash generate.sh
+        )
+        repo manifest -r > release/"$rom_fp"/manifest.xml
+        bash "$(dirname "$0")"/list-patches.sh
+        cp patches.zip release/"$rom_fp"/patches.zip
+    fi
+}
+
+function build_variant() {
+    lunch "$1"
+    make $extra_make_options BUILD_NUMBER="$rom_fp" installclean
+    make $extra_make_options BUILD_NUMBER="$rom_fp" -j "$jobs" systemimage
+    make $extra_make_options BUILD_NUMBER="$rom_fp" vndk-test-sepolicy
+    cp "$OUT"/system.img release/"$rom_fp"/system-"$2".img
+}
+
+function jack_env() {
+    RAM=$(free | awk '/^Mem:/{ printf("%0.f", $2/(1024^2))}') #calculating how much RAM (wow, such ram)
+    if [[ "$RAM" -lt 16 ]];then #if we're poor guys with less than 16gb
+	export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx"$((RAM -1))"G"
+    fi
+}
+
+parse_options "$@"
+get_rom_type "$@"
+get_variants "$@"
+
+if [[ -z "$mainrepo" || ${#variant_codes[*]} -eq 0 ]]; then
+    >&2 help
+    exit 1
+fi
+
+if [[ $choice == *"y"* ]];then
+init_release
+init_main_repo
+init_local_manifest
+init_patches
+sync_repo
+fi
+patch_things
+jack_env
+
+. build/envsetup.sh
+
+for (( idx=0; idx < ${#variant_codes[*]}; idx++ )); do
+    build_variant "${variant_codes[$idx]}" "${variant_names[$idx]}"
+done
